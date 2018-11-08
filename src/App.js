fa6c657e3070f5a599b235ee471d9033f29c94d8
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-08 16:04:54 -0500

    ejercicio v1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      valueForm: "",
+      numberForm: 0,
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.valueForm} onChange={this.updateValueForm.bind(this)}></textarea>
+        <div className="counter">{this.state.numberForm}</div>
       </div>
     );
   }
+  updateValueForm(event) {
+    
+      this.setState({
+      valueForm: event.target.value,
+      numberForm: this.state.valueForm.length
+    })
+  }
+  
 }
 
 export default App;
